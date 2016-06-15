128c5cdeab54da4b7ec0f8ee2023b16bf49b792c
Erik Hommel <hommel@ee-atwork.nl>
2016-06-15 16:07:21 +0200

    added import api for issue 3347

--- CRM/Casereports/Import.php
+++ CRM/Casereports/Import.php
@@ -16,6 +16,19 @@ class CRM_Casereports_Import {
   public $_rejectActivityTypeId = NULL;
   public $_briefingActivityTypeId = NULL;
 
+  public $_assessRepActivityTypeId = NULL;
+  public $_assessCCActivityTypeId = NULL;
+  public $_assessSCActivityTypeId = NULL;
+  public $_assessAnamonActivityTypeId = NULL;
+  public $_assessRepCustomTable = NULL;
+  public $_assessSCCustomTable = NULL;
+  public $_assessCCCustomTable = NULL;
+  public $_assessAnamonCustomTable = NULL;
+  public $_assessRepCustomColumn = NULL;
+  public $_assessSCCustomColumn = NULL;
+  public $_assessCCCustomColumn = NULL;
+  public $_assessAnamonCustomColumn = NULL;
+
   /**
    * CRM_Casereports_Import constructor.
    */
@@ -34,6 +47,18 @@ class CRM_Casereports_Import {
         $this->_ccColumn = $customField['column_name'];
       }
     }
+    $this->_assessRepActivityTypeId = $config->getAssessRepActivityTypeId();
+    $this->_assessSCActivityTypeId = $config->getAssessSCActivityTypeId();
+    $this->_assessCCActivityTypeId = $config->getAssessCCActivityTypeId();
+    $this->_assessAnamonActivityTypeId = $config->getAssessAnamonActivityTypeId();
+    $this->_assessRepCustomTable = $config->getAssessRepCustomTable();
+    $this->_assessSCCustomTable = $config->getAssessSCCustomTable();
+    $this->_assessCCCustomTable = $config->getAssessCCCustomTable();
+    $this->_assessAnamonCustomTable = $config->getAssessAnamonCustomTable();
+    $this->_assessRepCustomColumn = $config->getAssessRepCustomColumn();
+    $this->_assessSCCustomColumn = $config->getAssessSCCustomColumn();
+    $this->_assessCCCustomColumn = $config->getAssessCCCustomColumn();
+    $this->_assessAnamonCustomColumn = $config->getAssessAnamonCustomColumn();
   }
 
   /**
@@ -148,6 +173,139 @@ class CRM_Casereports_Import {
           3 => array(date('Ymd', strtotime($dao->activity_date_time)), 'String')
         );
         CRM_Core_DAO::executeQuery($insert, $values);
+      }
+    }
+  }
+
+  /**
+   * Method to import project intake data into civicrm_pum_case_reports for Project Intake report
+   *
+   * @param $caseId
+   * @access public
+   */
+  public function importProjectIntake($caseId) {
+    // get relevant activities
+    $actQuery = "SELECT act.id, act.activity_date_time, act.activity_type_id
+FROM civicrm_case_activity ca JOIN civicrm_activity act ON ca.activity_id = act.id AND act.is_current_revision = %6
+ AND act.is_test = %7 AND act.is_deleted = %7 AND act.activity_type_id IN (%1, %2, %3, %4) WHERE ca.case_id = %5";
+    $actParams = array(
+      1 => array($this->_assessSCActivityTypeId, 'Integer'),
+      2 => array($this->_assessRepActivityTypeId, 'Integer'),
+      3 => array($this->_assessCCActivityTypeId, 'Integer'),
+      4 => array($this->_assessAnamonActivityTypeId, 'Integer'),
+      5 => array($caseId, 'Integer'),
+      6 => array(1, 'Integer'),
+      7 => array(0, 'Integer'));
+    $actDao = CRM_Core_DAO::executeQuery($actQuery, $actParams);
+    $pumFields = array();
+    $pumParams = array();
+    $pumParams[1] = array($caseId, 'Integer');
+    $pumIndex = 1;
+    while ($actDao->fetch()) {
+      switch ($actDao->activity_type_id) {
+        case $this->_assessSCActivityTypeId:
+          if (!empty($actDao->activity_date_time)) {
+            $pumIndex++;
+            $clause = "assess_sc_date = %".$pumIndex;
+            if (!in_array($clause, $pumFields)) {
+              $pumFields[] = "assess_sc_date = %" . $pumIndex;
+              $pumParams[$pumIndex] = array(date('Y-m-d', strtotime($actDao->activity_date_time)), 'String');
+            } else {
+              $pumIndex--;
+            }
+          }
+          $scQuery = "SELECT ".$this->_assessSCCustomColumn." FROM ".$this->_assessSCCustomTable." WHERE entity_id = %1";
+          $scCustomer = CRM_Core_DAO::singleValueQuery($scQuery, array(1 => array($actDao->id, 'Integer')));
+          if (!empty($scCustomer)) {
+            $clause = "assess_sc_customer = %".$pumIndex;
+            if (!in_array($clause, $pumFields)) {
+              $pumFields[] = "assess_sc_customer = %" . $pumIndex;
+              $pumParams[$pumIndex] = array($scCustomer, 'String');
+            } else {
+              $pumIndex--;
+            }
+          }
+          break;
+        case $this->_assessRepActivityTypeId:
+          if (!empty($actDao->activity_date_time)) {
+            $clause = "assess_rep_date = %".$pumIndex;
+            if (!in_array($clause, $pumFields)) {
+              $pumFields[] = "assess_rep_date = %" . $pumIndex;
+              $pumParams[$pumIndex] = array(date('Y-m-d', strtotime($actDao->activity_date_time)), 'String');
+            } else {
+              $pumIndex--;
+            }
+          }
+          $repQuery = "SELECT ".$this->_assessRepCustomColumn." FROM ".$this->_assessRepCustomTable." WHERE entity_id = %1";
+          $repCustomer = CRM_Core_DAO::singleValueQuery($repQuery, array(1 => array($caseId, 'Integer')));
+          if (!empty($repCustomer)) {
+            $clause = "assess_rep_customer = %".$pumIndex;
+            if (!in_array($clause, $pumFields)) {
+              $pumFields[] = "assess_rep_customer = %" . $pumIndex;
+              $pumParams[$pumIndex] = array($repCustomer, 'String');
+            } else {
+              $pumIndex--;
+            }
+          }
+          break;
+        case $this->_assessCCActivityTypeId:
+          if (!empty($actDao->activity_date_time)) {
+            $clause = "assess_cc_date = %".$pumIndex;
+            if (!in_array($clause, $pumFields)) {
+              $pumFields[] = "assess_cc_date = %" . $pumIndex;
+              $pumParams[$pumIndex] = array(date('Y-m-d', strtotime($actDao->activity_date_time)), 'String');
+            } else {
+              $pumIndex--;
+            }
+          }
+          $ccQuery = "SELECT ".$this->_assessCCCustomColumn." FROM ".$this->_assessCCCustomTable." WHERE entity_id = %1";
+          $ccCustomer = CRM_Core_DAO::singleValueQuery($ccQuery, array(1 => array($actDao->id, 'Integer')));
+          if (!empty($ccCustomer)) {
+            $clause = "assess_cc_customer = %".$pumIndex;
+            if (!in_array($clause, $pumFields)) {
+              $pumFields[] = "assess_cc_customer = %" . $pumIndex;
+              $pumParams[$pumIndex] = array($ccCustomer, 'String');
+            } else {
+              $pumIndex--;
+            }
+          }
+          break;
+        case $this->_assessAnamonActivityTypeId:
+          if (!empty($actDao->activity_date_time)) {
+            $clause = "assess_anamon_date = %".$pumIndex;
+            if (!in_array($clause, $pumFields)) {
+              $pumFields[] = "assess_anamon_date = %" . $pumIndex;
+              $pumParams[$pumIndex] = array(date('Y-m-d', strtotime($actDao->activity_date_time)), 'String');
+            } else {
+              $pumIndex--;
+            }
+          }
+          $anamonQuery = "SELECT ".$this->_assessAnamonCustomColumn." FROM ".$this->_assessAnamonCustomTable." WHERE entity_id = %1";
+          $anaMonCustomer = CRM_Core_DAO::singleValueQuery($anamonQuery, array(1 => array($actDao->id, 'Integer')));
+          if (!empty($anaMonCustomer)) {
+            $clause = "assess_anamon_customer = %".$pumIndex;
+            if (!in_array($clause, $pumFields)) {
+              $pumFields[] = "assess_anamon_customer = %" . $pumIndex;
+              $pumParams[$pumIndex] = array($anaMonCustomer, 'String');
+            } else {
+              $pumIndex--;
+            }
+          }
+          break;
+      }
+    }
+    if (CRM_Casereports_Activity::caseExists($caseId)) {
+      if (!empty($pumFields)) {
+        $update = "UPDATE civicrm_pum_case_reports SET " . implode(', ', $pumFields) . " WHERE case_id = %1";
+        CRM_Core_DAO::executeQuery($update, $pumParams);
+      }
+    } else {
+      if (!empty($pumFields)) {
+        $insert = "INSERT INTO civicrm_pum_case_reports SET case_id = %1, " . implode(', ', $pumFields);
+        CRM_Core_DAO::executeQuery($insert, $pumParams);
+      } else {
+        $insert = "INSERT INTO civicrm_pum_case_reports SET case_id = %1";
+        CRM_Core_DAO::executeQuery($insert, $pumParams);
       }
     }
   }
--- api/v3/PumCase/Import.php
+++ api/v3/PumCase/Import.php
@@ -8,12 +8,29 @@
  * @see civicrm_api3_create_error
  * @throws API_Exception
  */
-function civicrm_api3_pum_case_import($params) {
+function civicrm_api3_pum_case_import($params)
+{
   $import = new CRM_Casereports_Import();
-  $daoCaseIds = CRM_Core_DAO::executeQuery('SELECT DISTINCT(case_id) FROM pum_my_main_activities');
-  while ($daoCaseIds->fetch()) {
-    $import->importAccepts($daoCaseIds->case_id);
-    $import->importBriefing($daoCaseIds->case_id);
+  // this part was required for import of main activities and briefing, no longer required so commented out
+  //$daoCaseIds = CRM_Core_DAO::executeQuery('SELECT DISTINCT(case_id) FROM pum_my_main_activities');
+  //while ($daoCaseIds->fetch()) {
+  //$import->importAccepts($daoCaseIds->case_id);
+  //$import->importBriefing($daoCaseIds->case_id);
+ //}
+  
+  // retrieve all active projectintake cases
+  $projectIntakeCaseTypeId = civicrm_api3('OptionValue', 'Getvalue', array(
+    'option_group_id' => 'case_type',
+    'name' => 'Projectintake',
+    'return' => 'value'
+  ));
+  $caseQuery = "SELECT id FROM civicrm_case WHERE case_type_id LIKE %1 AND is_deleted = %2";
+  $caseParams = array(
+    1 => array('%'.$projectIntakeCaseTypeId.'%', 'String'),
+    2 => array(0, 'Integer'));
+  $caseDao = CRM_Core_DAO::executeQuery($caseQuery, $caseParams);
+  while ($caseDao->fetch()) {
+    $import->importProjectIntake($caseDao->id);
   }
   return civicrm_api3_create_success(array('Existing pum case data imported into civicrm_pum_case_reports'), $params, 'PumCase', 'Import');
 }
